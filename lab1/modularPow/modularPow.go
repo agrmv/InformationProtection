5253e25edff200048100a3f10cdf7843aeead5ec
Aleksey Gromov <gromov-aleksey@outlook.com>
2019-09-10 21:25:35 +0700

    Возведения числа в степень по модулю

---
+++ lab1/modularPow/modularPow.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func modularPow(base *int64, exp *int64, modulus *int64) int64 {
+	for *exp != 0 {
+		if (*exp & 1) != 0 {
+			return (1 * *base) % *modulus
+		}
+		*base = (*base * *base) % *modulus
+		*exp >>= 1 //эквивалентно делению на 2^N
+	}
+	return 0
+}
+
+func main() {
+	fmt.Println("a^x mod p")
+	fmt.Println("Input a, x, p")
+	var a, x, p int64
+	if    _, err := fmt.Scan(&a, &x, &p);   err != nil {
+		log.Print("  Scan for a, x, p failed ", err)
+		return
+	}
+	fmt.Printf("\n\nResult: %d", modularPow(&a, &x, &p))
+}
