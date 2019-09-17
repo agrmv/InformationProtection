529084349ded171a2ee040e36909d4a68ca972f3
Aleksey <alexeygromov26@gmail.com>
2019-09-17 20:31:34 +0700

    Поправил формулу

--- lab1/modularPow/modularPow.go
+++ lab1/modularPow/modularPow.go
@@ -6,21 +6,22 @@ import (
 )
 
 func modularPow(base *int64, exp *int64, modulus *int64) int64 {
+	var result int64 = 1
 	for *exp != 0 {
 		if (*exp & 1) != 0 {
-			return (1 * *base) % *modulus
+			result = (result * *base) % *modulus
 		}
 		*base = (*base * *base) % *modulus
 		*exp >>= 1 //эквивалентно делению на 2^N
 	}
-	return 0
+	return result
 }
 
 func main() {
 	fmt.Println("a^x mod p")
 	fmt.Println("Input a, x, p")
 	var a, x, p int64
-	if    _, err := fmt.Scan(&a, &x, &p);   err != nil {
+	if _, err := fmt.Scan(&a, &x, &p); err != nil {
 		log.Print("  Scan for a, x, p failed ", err)
 		return
 	}
