9571966e2c76df66a01079d0ed834602a38d79c1
Aleksey Gromov <gromov-aleksey@outlook.com>
2019-10-16 12:39:51 +0700

    Рефакторинг эльгамаля

--- lab2/elgamal/Elgamal.go
+++ lab2/elgamal/Elgamal.go
@@ -19,14 +19,14 @@ type Keys struct {
 }
 
 type Alice struct {
-	message int64
+	message []int64
 	k       int64
 	a       int64
 	b       int64
 }
 
 type Bob struct {
-	decodedMessage int64
+	decodedMessage []byte
 	x              int64
 	y              int64
 }
@@ -41,27 +41,27 @@ func generateBobKeys(bob *Bob, P int64, g int64) {
 	bob.y = methods.ModularPow(g, bob.x, P)
 }
 
-func encrypt(alice *Alice, bob *Bob, P int64) {
-	alice.b = (int64(math.Pow(float64(bob.y), float64(alice.k))) * alice.message) % P
+func encrypt(alice *Alice, bob *Bob, P int64, temp int64) int64 {
+	return (int64(math.Pow(float64(bob.y), float64(alice.k))) * temp) % P
 }
 
-/*func EncryptMessage(file []byte, fileSize int64, alice *Alice, bob *Bob, P int64) {
+func EncryptMessage(file []byte, fileSize int64, alice *Alice, bob *Bob, P int64) {
 	alice.message = make([]int64, fileSize)
 	for i, v := range file {
 		alice.message[i] = encrypt(alice, bob, P, int64(v))
 	}
-}*/
-
-func decrypt(alice *Alice, bob *Bob, P int64) {
-	bob.decodedMessage = (alice.b * int64(math.Pow(float64(alice.a), float64(P-1-bob.x)))) % P
 }
 
-/*func DecryptMessage(alice *Alice, bob *Bob, P int64) {
+func decrypt(alice *Alice, bob *Bob, P int64, temp int64) int64 {
+	return (temp * int64(math.Pow(float64(alice.a), float64(P-1-bob.x)))) % P
+}
+
+func DecryptMessage(alice *Alice, bob *Bob, P int64) {
 	bob.decodedMessage = make([]byte, len(alice.message))
 	for i, v := range alice.message {
 		bob.decodedMessage[i] = byte(decrypt(alice, bob, P, v))
 	}
-}*/
+}
 
 func writeKeyToJson(path string, keys Keys) {
 	marshalKeys, _ := json.Marshal(keys)
@@ -79,24 +79,24 @@ func main() {
 	var A Alice
 	var B Bob
 	var P, g int64
-	A.message = 11
+	/*A.message = 11
 
 	for true {
 		P, _, g = methods.GeneratePQg(29)
 		if P > A.message {
 			break
 		}
-	}
+	}*/
 
 	generateAliceKeys(&A, P, g)
 	generateBobKeys(&B, P, g)
 	{
-		//writeKeyToJson("lab2/elgamal/resources/keys.json", Keys{Pair{A.a, A.k}, Pair{B.x, B.y}})
-		encrypt(&A, &B, P)
-		decrypt(&A, &B, P)
-		//file, fileSize := methods.ReadFile("lab2/resourcesGlobal/test.jpg")
-		//EncryptMessage(file, fileSize, &A, &B, P)
-		//DecryptMessage(&A, &B, P)
+		writeKeyToJson("lab2/elgamal/resources/keys.json", Keys{Pair{A.a, A.k}, Pair{B.x, B.y}})
+		//encrypt(&A, &B, P)
+		//decrypt(&A, &B, P)
+		file, fileSize := methods.ReadFile("lab2/resourcesGlobal/test.jpg")
+		EncryptMessage(file, fileSize, &A, &B, P)
+		DecryptMessage(&A, &B, P)
 		//methods.WriteFile("lab2/elgamal/resources/decrypt.jpg", B.decodedMessage)
 	}
 
