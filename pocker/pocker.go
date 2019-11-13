b818e929dbcb948003b31f62ca66929bd6a2b21c
Aleksey Gromov <gromov-aleksey@outlook.com>
2019-11-13 09:11:59 +0700

    ЛАБА ГОТОВА НА 90%!!!

--- pocker/pocker.go
+++ pocker/pocker.go
@@ -10,8 +10,8 @@ type Player struct {
 	P           int64
 	C           int64
 	D           int64
-	cartEncrypt []int64
-	cartDecrypt []int64
+	cardEncrypt []int64
+	cardDecrypt []int64
 }
 
 func GenerateKeys(P int64) (int64, int64) {
@@ -61,17 +61,25 @@ func initPlayers(p, n int64) []Player {
 	for i := int64(0); i < n; i++ {
 		player := initPlayer(p)
 		players = append(players, player)
-		fmt.Println("player N", i, ": ", player)
+		fmt.Println("player ", i, ": ", player)
 	}
 	return players
 }
 
 func (p *Player) encryptDeck(deck []int64) []int64 {
-	encodeDeck := make([]int64, len(deck))
+	encryptDeck := make([]int64, len(deck))
 	for i, item := range deck {
-		encodeDeck[i] = methods.ModularPow(item, p.C, p.P)
+		encryptDeck[i] = methods.ModularPow(item, p.C, p.P)
 	}
-	return encodeDeck
+	return encryptDeck
+}
+
+func (p *Player) decryptDeck(deck []int64) []int64 {
+	decryptDeck := make([]int64, len(deck))
+	for i, item := range deck {
+		decryptDeck[i] = methods.ModularPow(item, p.D, p.P)
+	}
+	return decryptDeck
 }
 
 func encryptAllDeck(players []Player, encrypt []int64) []int64 {
@@ -79,6 +87,13 @@ func encryptAllDeck(players []Player, encrypt []int64) []int64 {
 		encrypt = player.encryptDeck(encrypt)
 	}
 	return encrypt
+}
+
+func decryptAllDeck(players []Player, decrypt []int64) []int64 {
+	for _, player := range players {
+		decrypt = player.decryptDeck(decrypt)
+	}
+	return decrypt
 }
 
 func distributeCards(players *[]Player, deck *[]int64) {
@@ -89,7 +104,21 @@ func distributeCards(players *[]Player, deck *[]int64) {
 				fmt.Println()
 				break
 			}
-			(&(*players)[j]).cartEncrypt = append((&(*players)[j]).cartEncrypt, (*deck)[len(*deck)-1])
+			(&(*players)[j]).cardEncrypt = append((&(*players)[j]).cardEncrypt, (*deck)[len(*deck)-1])
+			*deck = (*deck)[:len(*deck)-1]
+		}
+	}
+}
+
+func distributeDecryptedCards(players *[]Player, deck *[]int64) {
+	//2 - число карт на игрока
+	for i := 0; i < 2; i++ {
+		for j, _ := range *players {
+			if len(*deck)-1 < 0 {
+				fmt.Println()
+				break
+			}
+			(&(*players)[j]).cardDecrypt = append((&(*players)[j]).cardDecrypt, (*deck)[len(*deck)-1])
 			*deck = (*deck)[:len(*deck)-1]
 		}
 	}
@@ -105,8 +134,16 @@ func main() {
 	players := initPlayers(P, 3)
 	encryptDeck := encryptAllDeck(players, arr)
 	fmt.Println("encrypt deck:", encryptDeck)
+
+	decryptDeck := decryptAllDeck(players, encryptDeck)
+	fmt.Println("decrypt deck:", decryptDeck)
+
 	distributeCards(&players, &encryptDeck)
-	fmt.Println("players", players)
+	fmt.Println("players with encrypted", players)
+
+	//костыль
+	distributeDecryptedCards(&players, &decryptDeck)
+	fmt.Println("players with decrypted", players)
 
 	//TODO DECRYPT
 	//TODO change num to pic/text
